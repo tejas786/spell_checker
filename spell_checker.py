470ff184d6b24e4de71b615beb1f6c96d5d8474d
tejas786 <57308390+tejas786@users.noreply.github.com>
2019-11-04 12:42:43 +0530

    Create spell_checker.py

---
+++ spell_checker.py
@@ -0,0 +1,20 @@
+from spellchecker import SpellChecker
+
+spell = SpellChecker()
+
+def create_list(s):
+    misp=[]
+    for words in s.casefold().split():
+	misp.append(words)
+    return misp
+
+def word_check(str_word):
+    misspelled = spell.unknown(str_word)
+    for word in misspelled:
+        if word != spell.correction(word):
+            suggestion=spell.correction(word)
+            print(f'Did you mean {"," .join(suggestion)} instead of {word}?')
+
+str_in=input('Input string : ')
+str_to_word=create_list(str_in)
+word_check(str_to_word)
